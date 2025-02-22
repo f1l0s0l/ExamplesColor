3acc6ad3384988bf5176a5713cbe936378fe258b
Илья Сидорик <iliasidorik@MacBook-Pro-Ila-2.local>
2025-02-22 15:55:56 +0300

    create new project

---
+++ GenerateColors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenerateColors
+//
+//  Created by Илья Сидорик on 22.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
